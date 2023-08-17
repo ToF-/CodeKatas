a1509590bd860434710c5cdb5093d53dae2b5676
ToF_ <cthibauttof@gmail.com>
2023-08-17 12:16:03 +0200

    removed target

--- rust/bubble-sort-once/target/debug/deps/bubble_sort_once-2c3088d49e856679.d
+++
@@ -1,5 +0,0 @@
-/home/tof/Coding/CodeKatas/rust/bubble-sort-once/target/debug/deps/bubble_sort_once-2c3088d49e856679: src/lib.rs
-
-/home/tof/Coding/CodeKatas/rust/bubble-sort-once/target/debug/deps/bubble_sort_once-2c3088d49e856679.d: src/lib.rs
-
-src/lib.rs:
