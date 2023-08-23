7885381b445aa50c3810ec006614be4ef2225cc0
Christophe Thibaut <cthibaut@octo.com>
2023-08-23 23:48:33 +0200

    wip forth word-ladder in a mess

--- word-ladder/code/forth/tests.fs
+++ word-ladder/code/forth/tests.fs
@@ -1,27 +1,30 @@
 \ tests.fs
 REQUIRE ffl/tst.fs
 REQUIRE word-ladder.fs
+PAGE
 
+256 WORD-LIST MAIN
 T{ .( clear dictionnary, not finding any word ) CR
-    S" DOG" WORD-EXIST? FALSE ?S
+    S" DOG" MAIN FIND-WORD-L FALSE ?S
 }T
 T{ .( adding words, finding the words exist ) CR
-    CLEAR-DICTIONARY
-    S" DOG" ADD-WORD
-    S" FOG" ADD-WORD
-    S" COG" ADD-WORD
-    S" DOG" WORD-EXIST? TRUE ?S
-    S" FOO" WORD-EXIST? FALSE ?S
-    S" COG" WORD-EXIST? TRUE ?S
+    MAIN WORD-LIST-CLEAR
+    S" DOG" MAIN dbg ADD-WORD-L
+    S" FOG" MAIN ADD-WORD-L
+    S" COG" MAIN ADD-WORD-L
+    S" DOG" MAIN FIND-WORD-L TRUE ?S COUNT S" DOG" ?STR
+    S" FOO" MAIN FIND-WORD-L FALSE ?S
+    S" COG" MAIN FIND-WORD-L TRUE ?S COUNT S" COG" ?STR
 }T
+BYE
 T{ .( adding words from a file ) CR
     CLEAR-DICTIONARY
     S" ../../src/3-letter-words.txt" READ-WORDS
     S" ../../src/4-letter-words.txt" READ-WORDS
     S" ../../src/5-letter-words.txt" READ-WORDS
-    S" ABOUT" WORD-EXIST? TRUE ?S
-    S" ZOOMS" WORD-EXIST? TRUE ?S
-    S" FUBAR" WORD-EXIST? FALSE ?S
+    S" about" WORD-EXIST? TRUE ?S
+    S" zooms" WORD-EXIST? TRUE ?S
+    S" fubar" WORD-EXIST? FALSE ?S
 }T
 T{ .( two words are adjacent if they are the same size and differ by only one letter ) CR
     S" DOG" S" HORSE" ADJACENT? FALSE ?S
--- word-ladder/code/forth/word-ladder.fs
+++ word-ladder/code/forth/word-ladder.fs
@@ -1,15 +1,76 @@
 \ word-ladder.fs
-50000 CONSTANT MAX-WORD-BUFFER
-CREATE WORD-BUFFER MAX-WORD-BUFFER ALLOT
+50000 CONSTANT MAX-WORDS
+
+: WORD-LIST ( <ńame> u -- addr )
+    CREATE HERE 2 CELLS + OVER + , HERE CELL + ,
+    ALLOT ;
+
+: WORD-LIST-MAX ( list -- n )
+    @ ;
+
+: WORD-LIST>WORDS ( list -- addr )
+    2 CELLS + ;
+
+: WORD-LIST-NEXT ( list -- addr )
+    CELL+ ;
+
+: WORD-LIST-NEXT@ ( list -- addr )
+    WORD-LIST-NEXT @ ;
+
+: WORD-LIST-NEXT! ( list,count -- )
+    1+ SWAP WORD-LIST-NEXT +! ;
+
+: WORD-LIST-CLEAR ( list -- )
+    DUP WORD-LIST>WORDS
+    OVER WORD-LIST-MAX OVER - ERASE
+    DUP WORD-LIST>WORDS SWAP WORD-LIST-NEXT  ;
+
+: WORD-LIST-EMPTY? ( list -- f )
+    DUP WORD-LIST-NEXT@
+    SWAP WORD-LIST>WORDS = ;
+
+: (ADD-WORD-L) ( addr,count,list -- )
+    2DUP WORD-LIST-NEXT@ -ROT      \ addr,count,next@,list,count
+    WORD-LIST-NEXT!                \ addr,count,next@
+    2DUP C! 1+ SWAP CMOVE ;
+
+: WORD-LIST-CAPACITY? ( count,list -- f )
+    DUP WORD-LIST-MAX -ROT
+    WORD-LIST-NEXT@ + > ;
+
+: ADD-WORD-L ( addr,count, list -- )
+    OVER 1+ OVER
+    WORD-LIST-CAPACITY? IF
+        (ADD-WORD-L)
+    ELSE
+        S" word-list out of capacity" 
+        EXCEPTION THROW 
+    THEN ;
+
+: FIND-WORD-L ( addr, count, list -- addr|f )
+    DUP WORD-LIST-EMPTY? 0= IF
+        FALSE OVER DUP WORD-LIST-NEXT@ 
+        OVER WORD-LIST>WORDS DO
+            DROP 2DUP I COUNT COMPARE
+            IF FALSE ELSE I LEAVE THEN
+            I C@ 1+
+        +LOOP
+    ELSE
+        DROP FALSE
+    THEN
+    -ROT 2DROP ;
+
+
+CREATE MAIN-LIST MAX-WORDS ALLOT
 VARIABLE NEXT-WORD
-WORD-BUFFER NEXT-WORD !
+MAIN-LIST NEXT-WORD !
 
 : CLEAR-DICTIONARY
-    WORD-BUFFER MAX-WORD-BUFFER ERASE
-    WORD-BUFFER NEXT-WORD ! ;
+    MAIN-LIST MAX-WORDS ERASE
+    MAIN-LIST NEXT-WORD ! ;
 
 : DICTIONARY-EMPTY? ( -- f )
-    NEXT-WORD @ WORD-BUFFER = ;
+    NEXT-WORD @ MAIN-LIST = ;
 
 : ADD-WORD ( addr,count -- )
     NEXT-WORD @
@@ -17,8 +78,8 @@ WORD-BUFFER NEXT-WORD !
     1+ SWAP CMOVE
     NEXT-WORD DUP @ DUP C@ 1+ + SWAP ! ;
    
-: (WORD-EXIST?) ( addr, count -- f )
-    FALSE NEXT-WORD @ WORD-BUFFER 
+: (WORD-EXIST?) ( addr, count, list -- f )
+    FALSE NEXT-WORD @ MAIN-LIST 
         DO
         DROP 2DUP I COUNT COMPARE 
         0= IF
@@ -71,9 +132,9 @@ CREATE LINE-BUFFER LINE-MAX ALLOT
     CLOSE-FILE THROW ;
 
 : .(ADJACENTS) 
-    NEXT-WORD @ WORD-BUFFER DO
+    NEXT-WORD @ MAIN-LIST DO
         I COUNT 2DUP TYPE SPACE [CHAR] { EMIT SPACE
-        NEXT-WORD @ WORD-BUFFER DO
+        NEXT-WORD @ MAIN-LIST DO
             I COUNT 2OVER 2OVER
             ADJACENT? IF TYPE SPACE ELSE 2DROP THEN
             I C@ 1+ 
