f5d1c9fb9842e93206217a89ae86d989dd927654
Christophe Thibaut <cthibaut@octo.com>
2023-08-24 08:08:29 +0200

    refactoring #2

--- word-ladder/code/forth/tests.fs
+++ word-ladder/code/forth/tests.fs
@@ -4,7 +4,7 @@ REQUIRE word-ladder.fs
 PAGE
 
 T{ .( after creation wl is empty ) CR
-    256 WL-CREATE my-list
+    100000 WL-CREATE my-list
     my-list WL-EMPTY? TRUE ?S
 }T
 T{ .( when empty wl cannot find a word ) CR
@@ -15,30 +15,20 @@ T{ .( after adding a word, wl can find the word ) CR
     S" DOG" my-list WL-FIND
     COUNT S" DOG" ?STR
 }T
-BYE 
-T{ .( clear dictionnary, not finding any word ) CR
+T{ .( after emptying wl cannot find any word ) CR
+    my-list WL-EMPTY
     S" DOG" my-list WL-FIND FALSE ?S
 }T
-BYE
-T{ .( adding words, finding the words exist ) CR
-    MAIN WORD-LIST-CLEAR
-    S" DOG" MAIN dbg ADD-WORD-L
-    S" FOG" MAIN ADD-WORD-L
-    S" COG" MAIN ADD-WORD-L
-    S" DOG" MAIN FIND-WORD-L TRUE ?S COUNT S" DOG" ?STR
-    S" FOO" MAIN FIND-WORD-L FALSE ?S
-    S" COG" MAIN FIND-WORD-L TRUE ?S COUNT S" COG" ?STR
+T{ .( after reading words from a file words can be found ) CR
+    my-list WL-EMPTY
+    S" ../../src/3-letter-words.txt" my-list WL-READ-FILE
+    S" ../../src/4-letter-words.txt" my-list WL-READ-FILE
+    S" ../../src/5-letter-words.txt" my-list WL-READ-FILE
+    S" fog" my-list WL-FIND 0= FALSE ?S
+    S" zooms" my-list WL-FIND 0= FALSE ?S 
+    S" fubar" my-list WL-FIND FALSE ?S
 }T
 BYE
-T{ .( adding words from a file ) CR
-    CLEAR-DICTIONARY
-    S" ../../src/3-letter-words.txt" READ-WORDS
-    S" ../../src/4-letter-words.txt" READ-WORDS
-    S" ../../src/5-letter-words.txt" READ-WORDS
-    S" about" WORD-EXIST? TRUE ?S
-    S" zooms" WORD-EXIST? TRUE ?S
-    S" fubar" WORD-EXIST? FALSE ?S
-}T
 T{ .( two words are adjacent if they are the same size and differ by only one letter ) CR
     S" DOG" S" HORSE" ADJACENT? FALSE ?S
     S" DOG" S" FOG" ADJACENT? TRUE ?S
--- word-ladder/code/forth/word-ladder.fs
+++ word-ladder/code/forth/word-ladder.fs
@@ -3,10 +3,13 @@
 
 
 : WL-CREATE ( <name> u )
-    CREATE DUP , 0 , ALLOT ; 
+    CREATE DUP , 0 , ALLOT ;
 
 : WL-EMPTY? ( wl -- f )
-    2 CELLS + @ 0= ;
+    CELL + @ 0= ;
+
+: WL-EMPTY ( wl -- )
+    CELL+ OFF ;
 
 : WL-CAPACITY? ( u,wl -- f )
     DUP CELL+ @ ROT 1+ + SWAP @ < ;
@@ -16,11 +19,11 @@
 
 : WL-NEXT ( wl -- adr )
     DUP WL>WORDS SWAP CELL+ @ + ;
-    
+
 : WL-NEXT! ( u,wl -- adr )
     SWAP 1+ SWAP
     CELL+ +! ;
-        
+
 : (WL-ADD) ( adr,u,wl -- )
     2>R
     2R@ WL-NEXT 2DUP C!
@@ -29,10 +32,10 @@
 
 : WL-ADD ( adr,u,wl -- )
     2DUP WL-CAPACITY? IF
-       (WL-ADD) 
+       (WL-ADD)
     ELSE
         DROP 2DROP
-        S" wl-add: out of capacity" 
+        S" wl-add: out of capacity"
         EXCEPTION THROW
     THEN ;
 
@@ -40,7 +43,7 @@
     DUP WL-NEXT
     SWAP WL>WORDS
     FALSE -ROT DO
-        DROP 2DUP 
+        DROP 2DUP
         I COUNT COMPARE IF
             FALSE
         ELSE
@@ -48,8 +51,8 @@
         THEN
         I C@ 1+
     +LOOP
-    -ROT 2DROP ; 
-    
+    -ROT 2DROP ;
+
 : WL-FIND ( adr,u,wl -- ad|f )
     DUP WL-EMPTY? IF
         DROP 2DROP FALSE
@@ -57,147 +60,52 @@
         (WL-FIND)
     THEN ;
 
-: WORD-LIST ( <name> u -- addr )
-    CREATE HERE 2 CELLS + OVER + , HERE CELL + ,
-    ALLOT ;
-
-: WORD-LIST-MAX ( list -- n )
-    @ ;
-
-: WORD-LIST>WORDS ( list -- addr )
-    2 CELLS + ;
-
-: WORD-LIST-NEXT ( list -- addr )
-    CELL+ ;
-
-: WORD-LIST-NEXT@ ( list -- addr )
-    WORD-LIST-NEXT @ ;
-
-: WORD-LIST-NEXT! ( list,count -- )
-    1+ SWAP WORD-LIST-NEXT +! ;
-
-: WORD-LIST-CLEAR ( list -- )
-    DUP WORD-LIST>WORDS
-    OVER WORD-LIST-MAX OVER - ERASE
-    DUP WORD-LIST>WORDS SWAP WORD-LIST-NEXT  ;
-
-: WORD-LIST-EMPTY? ( list -- f )
-    DUP WORD-LIST-NEXT@
-    SWAP WORD-LIST>WORDS = ;
-
-: (ADD-WORD-L) ( addr,count,list -- )
-    2DUP WORD-LIST-NEXT@ -ROT      \ addr,count,next@,list,count
-    WORD-LIST-NEXT!                \ addr,count,next@
-    2DUP C! 1+ SWAP CMOVE ;
-
-: WORD-LIST-CAPACITY? ( count,list -- f )
-    DUP WORD-LIST-MAX -ROT
-    WORD-LIST-NEXT@ + > ;
-
-: ADD-WORD-L ( addr,count, list -- )
-    OVER 1+ OVER
-    WORD-LIST-CAPACITY? IF
-        (ADD-WORD-L)
-    ELSE
-        S" word-list out of capacity" 
-        EXCEPTION THROW 
-    THEN ;
-
-: FIND-WORD-L ( addr, count, list -- addr|f )
-    DUP WORD-LIST-EMPTY? 0= IF
-        FALSE OVER DUP WORD-LIST-NEXT@ 
-        OVER WORD-LIST>WORDS DO
-            DROP 2DUP I COUNT COMPARE
-            IF FALSE ELSE I LEAVE THEN
-            I C@ 1+
-        +LOOP
-    ELSE
-        DROP FALSE
-    THEN
-    -ROT 2DROP ;
-
-
-CREATE MAIN-LIST MAX-WORDS ALLOT
-VARIABLE NEXT-WORD
-MAIN-LIST NEXT-WORD !
-
-: CLEAR-DICTIONARY
-    MAIN-LIST MAX-WORDS ERASE
-    MAIN-LIST NEXT-WORD ! ;
-
-: DICTIONARY-EMPTY? ( -- f )
-    NEXT-WORD @ MAIN-LIST = ;
-
-: ADD-WORD ( addr,count -- )
-    NEXT-WORD @
-    2DUP C!
-    1+ SWAP CMOVE
-    NEXT-WORD DUP @ DUP C@ 1+ + SWAP ! ;
-   
-: (WORD-EXIST?) ( addr, count, list -- f )
-    FALSE NEXT-WORD @ MAIN-LIST 
-        DO
-        DROP 2DUP I COUNT COMPARE 
-        0= IF
-            TRUE LEAVE
-        ELSE
-            FALSE
-        THEN
-        I C@ 1+
-    +LOOP
-    -ROT 2DROP ;
-
-: WORD-EXIST? ( addr, count -- f )
-    DICTIONARY-EMPTY? IF
-        2DROP FALSE
-    ELSE
-        (WORD-EXIST?)
-    THEN ;
-
-: SAME-SIZE? ( addr,count,addr,count -- f )
-    NIP ROT DROP = ;
-
-: (ADJACENT?) ( addr,count,addr,count -- f )
-    DROP -ROT 0 -ROT 
-    OVER + SWAP DO
-        OVER C@ I C@ <> IF 1+ THEN
-        SWAP 1+ SWAP
-    LOOP 
-    NIP
-    1 = ;
-
-: ADJACENT? ( addr,count,addr,count )
-    2OVER 2OVER SAME-SIZE? IF
-        (ADJACENT?)
-    ELSE
-        2DROP 2DROP FALSE
-    THEN ;
-
-
 1000 CONSTANT LINE-MAX
 CREATE LINE-BUFFER LINE-MAX ALLOT
 
-: READ-WORDS ( addr,count -- )
+: WL-READ-FILE ( adr,u,wl -- )
+    >R
     R/O OPEN-FILE THROW
     BEGIN
         DUP LINE-BUFFER LINE-MAX
         ROT READ-LINE THROW
     WHILE
-        LINE-BUFFER SWAP ADD-WORD
+        LINE-BUFFER SWAP R@ WL-ADD
     REPEAT DROP
-    CLOSE-FILE THROW ;
+    CLOSE-FILE THROW R> DROP ;
 
-: .(ADJACENTS) 
-    NEXT-WORD @ MAIN-LIST DO
-        I COUNT 2DUP TYPE SPACE [CHAR] { EMIT SPACE
-        NEXT-WORD @ MAIN-LIST DO
-            I COUNT 2OVER 2OVER
-            ADJACENT? IF TYPE SPACE ELSE 2DROP THEN
-            I C@ 1+ 
-        +LOOP
-        [CHAR] } EMIT CR
-        NIP 1+
-    +LOOP ;
 
-: .ADJACENTS
-    DICTIONARY-EMPTY? 0= IF .(ADJACENTS) THEN ;
+\ : SAME-SIZE? ( addr,count,addr,count -- f )
+\     NIP ROT DROP = ;
+\ 
+\ : (ADJACENT?) ( addr,count,addr,count -- f )
+\     DROP -ROT 0 -ROT
+\     OVER + SWAP DO
+\         OVER C@ I C@ <> IF 1+ THEN
+\         SWAP 1+ SWAP
+\     LOOP
+\     NIP
+\     1 = ;
+\ 
+\ : ADJACENT? ( addr,count,addr,count )
+\     2OVER 2OVER SAME-SIZE? IF
+\         (ADJACENT?)
+\     ELSE
+\         2DROP 2DROP FALSE
+\     THEN ;
+\ 
+\ : .(ADJACENTS)
+\     NEXT-WORD @ MAIN-LIST DO
+\         I COUNT 2DUP TYPE SPACE [CHAR] { EMIT SPACE
+\         NEXT-WORD @ MAIN-LIST DO
+\             I COUNT 2OVER 2OVER
+\             ADJACENT? IF TYPE SPACE ELSE 2DROP THEN
+\             I C@ 1+
+\         +LOOP
+\         [CHAR] } EMIT CR
+\         NIP 1+
+\     +LOOP ;
+\ 
+\ : .ADJACENTS
+\     DICTIONARY-EMPTY? 0= IF .(ADJACENTS) THEN ;
+\     CLOSE-FILE THROW R> DROP ;
