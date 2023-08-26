d6670eedcf25f8283cdb2eb9d74c6822ffec6c35
ToF_ <cthibauttof@gmail.com>
2023-08-26 18:41:03 +0200

    word-ladder forth clear values

--- word-ladder/code/forth/tests.fs
+++ word-ladder/code/forth/tests.fs
@@ -25,22 +25,42 @@ T{ .( two S>KEYs are adjacent if their string are.) CR
     s" bag" S>KEY s" fog" S>KEY KEY-ADJACENT? ?FALSE
 }T
 
+T{ .( a string as key can be given from input flow.) CR
+    KEY" dog" s" dog" S>KEY ?S
+}T
 T{ .( words can be added in and retrieved from a word dictionary. ) CR
     WORD-DICTIONARY dict
-    s" dog" dict FIND-WORD ?FALSE
-    4807 s" dog" dict ADD-WORD
-    s" dog" dict FIND-WORD ?TRUE 4807 ?S
-    1000 s" cat" dict ADD-WORD
-    4096 s" dog" dict ADD-WORD
-    s" dog" dict FIND-WORD ?TRUE 4096 ?S
+    KEY" dog" dict FIND-WORD ?FALSE
+    4807 KEY" dog" dict ADD-WORD
+    KEY" dog" dict FIND-WORD ?TRUE 4807 ?S
+    1000 KEY" cat" dict ADD-WORD
+    4096 KEY" dog" dict ADD-WORD
+    KEY" dog" dict FIND-WORD ?TRUE 4096 ?S
     dict ACT-LENGTH@ 2 ?S
 }T
 
 T{ .( words can be read from a file into a dictionary. ) CR
     s" sample.txt" dict READ-WORDS
-    s" bee" dict FIND-WORD ?TRUE DROP
-    s" wax" dict FIND-WORD ?TRUE DROP
+    KEY" bee" dict FIND-WORD ?TRUE DROP
+    KEY" wax" dict FIND-WORD ?TRUE DROP
 }T
 
+dict ACT-CLEAR
+s" small.txt" dict READ-WORDS
+
+T{ .( after visiting the word, the word is visited. ) CR
+    KEY" eel" dict FIND-WORD ?TRUE ?FALSE
+    KEY" eel" dict VISIT-WORD
+    KEY" eel" dict FIND-WORD ?TRUE ?TRUE
+}T
+
+T{ .( after clearing word values all words values are set fo false. ) CR
+    KEY" cat" dict VISIT-WORD
+    KEY" dog" dict VISIT-WORD
+    dict CLEAR-WORD-VALUES
+    KEY" eel" dict FIND-WORD ?TRUE ?FALSE
+    KEY" cat" dict FIND-WORD ?TRUE ?FALSE
+    KEY" dog" dict FIND-WORD ?TRUE ?FALSE
+}T  
 BYE
 
