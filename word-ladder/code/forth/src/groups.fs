871125263028f55ce49fc1ca6ffff98c3d295f35
Christophe Thibaut <cthibaut@octo.com>
2023-08-29 00:15:03 +0200

    wordladder forth groups and letter set

---
+++ word-ladder/code/forth/src/groups.fs
@@ -0,0 +1,35 @@
+\ groups.fs
+
+: S-COPY ( a,l,dest -- )
+    2DUP C!
+    1+ SWAP CMOVE ;
+
+: NTH-C! ( c,i,dest -- )
+    + C! ;
+
+: S>NTH-GROUP ( ad,l,i,dest -- dest+1,l )
+    DUP >R 2SWAP ROT      \ i,ad,l,dest
+    S-COPY                \ i
+    R@ [CHAR] ~ -ROT      \ c,i,dest
+    1+ NTH-C!
+    R> COUNT ;
+
+0 CONSTANT LS-EMPTY
+
+: NTH-BIT ( n -- cell )
+    1 SWAP LSHIFT ;
+    
+: LS-ADD-LETTER ( c,ls  -- ls' )
+    SWAP [CHAR] ` - NTH-BIT OR ;
+
+: LS>S ( ls,pad -- ad,l )
+    DUP ROT
+    27 1 DO
+        I NTH-BIT OVER AND IF
+            SWAP 1+
+            I [CHAR] ` + OVER C!
+            SWAP
+        THEN
+    LOOP 
+    DROP OVER - 
+    OVER C! COUNT ;
--- word-ladder/code/forth/test/tests.fs
+++ word-ladder/code/forth/test/tests.fs
@@ -5,4 +5,5 @@ REQUIRE test-wordkey.fs
 REQUIRE test-queue.fs
 REQUIRE test-dict.fs
 REQUIRE test-word-graph.fs
+REQUIRE test-groups.fs
 BYE
