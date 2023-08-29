77d953108efe92288aabda3e706f5e739e3566c7
Christophe Thibaut <cthibaut@octo.com>
2023-08-29 22:04:12 +0200

    refactoring

--- word-ladder/code/forth/test/tests.fs
+++ word-ladder/code/forth/test/tests.fs
@@ -2,9 +2,8 @@
 REQUIRE ffl/tst.fs
 PAGE
 REQUIRE wl-word-test.fs
+REQUIRE wl-queue-test.fs
 
-\ REQUIRE test-adjacent.fs
-\ REQUIRE test-wordkey.fs
 \ REQUIRE test-queue.fs
 \ REQUIRE test-dict.fs
 \ REQUIRE test-word-graph.fs
