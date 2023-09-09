890414ae53cee8a796e76f15367fa3bd8014d5a9
ToF_ <cthibauttof@gmail.com>
2023-09-09 14:06:41 +0200

    wordladder forth v2 queue

--- word-ladder/forth/src/queue.fs
+++ word-ladder/forth/src/queue.fs
@@ -1,2 +1,26 @@
 \ queue.fs
 
+REQUIRE ffl/car.fs
+
+: QUEUE ( n <name> -- )
+    0 CAR-CREATE ;
+
+: Q-EMPTY? ( q -- f )
+   CAR-LENGTH@ 0= ;
+
+: Q-HEAD@ ( q -- n )
+    0 SWAP CAR-GET ;
+
+: Q-APPEND ( n,q -- )
+    CAR-APPEND ;
+
+: Q-POP ( q -- n )
+    0 SWAP CAR-DELETE ;
+
+: Q-EMPTY ( q -- )
+    BEGIN
+        DUP Q-EMPTY? 0= WHILE
+        DUP Q-POP DROP
+    REPEAT DROP ;
+
+
--- word-ladder/forth/test/queue-test.fs
+++ word-ladder/forth/test/queue-test.fs
@@ -3,3 +3,33 @@
 REQUIRE ffl/tst.fs
 REQUIRE ../src/queue.fs
 
+CR .( queue ) CR
+.(   after adding an element the queue is not empty. ) CR
+T{
+    QUEUE q
+    q Q-EMPTY? ?TRUE
+    4807 q Q-APPEND
+    q Q-EMPTY? ?FALSE
+}T
+.(   after appending elements the elements can be popped in a FIFO manner. ) CR
+T{ 
+    23 q Q-APPEND
+    17 q Q-APPEND
+    q Q-POP 4807 ?S
+    q Q-POP 23 ?S
+    q Q-POP 17 ?S
+}T
+.(   after emptying the queue, the queue is empty. ) CR
+T{ 
+    23 q Q-APPEND
+    17 q Q-APPEND
+    q Q-EMPTY
+    q Q-EMPTY? ?TRUE
+}T 
+.(   the head of the queue can be accessed without popping it. ) CR
+T{
+    4807 q Q-APPEND
+    q Q-HEAD@ 4807 ?S
+    q Q-EMPTY? ?FALSE
+}T
+
